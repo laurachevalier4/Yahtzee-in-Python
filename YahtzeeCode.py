99805cf162db6fdefb8e59e993433f695d835d9a
laurachevalier4 <laurachevalier4@yahoo.com>
2016-03-25 11:10:15 -0400

    Update YahtzeeCode.py

--- YahtzeeCode.py
+++ YahtzeeCode.py
@@ -9,6 +9,7 @@ def score(list_of_dice, category):
     dice_counting = [0, 0, 0, 0, 0, 0]
     score = 0
     secondary_score = 0
+    total = 0
     for element in score_board:
         if element[0] == category.lower():
             if category.lower() == "ones":
@@ -51,12 +52,12 @@ def score(list_of_dice, category):
                         dice_counting[5] += 1
                 if category.lower() == "3 of a kind":
                     for count in dice_counting:
-                        if count == 3:
+                        if count >= 3:
                             for die in list_of_dice:
                                 score += die
                 elif category.lower() == "4 of a kind":
                     for count in dice_counting:
-                        if count == 4:
+                        if count >= 4:
                             for die in list_of_dice:
                                 score += die
                 elif category.lower() == "full house":
@@ -91,11 +92,10 @@ def score(list_of_dice, category):
                         score += 40
                 elif category.lower() == "yahtzee":
                     for num in list_of_dice:
-                        if list_of_dice.index(num) == (list_of_dice[list_of_dice.index(num) - 1]):
-                            score += num
-                            continue
-                        else:
-                            break                       
+                        total += num
+                        if (total / 5) == num:
+                            score += 50
+                            print("\n***WOOT!***\n")
                 elif category.lower() == "chance":
                     for num in list_of_dice:
                         score += num               
@@ -109,6 +109,8 @@ def main():
     score_board = [["ones", 0], ["twos", 0], ["threes", 0], ["fours", 0], ["fives", 0], ["sixes", 0], ["3 of a kind", 0], ["4 of a kind", 0], ["full house", 0], ["small straight", 0], ["large straight", 0], ["yahtzee", 0], ["chance", 0]]
     check_if_category = ["ones", "twos", "threes", "fours", "fives", "sixes", "3 of a kind", "4 of a kind", "full house", "small straight", "large straight", "yahtzee", "chance"]
     used_score_categories = []
+    saved_dice = []
+    return_dice = []
     total_score = 0
     bonus = 0
     import random
@@ -122,6 +124,8 @@ def main():
                 dice[count] = random.randint(1,6)
                 count += 1
             print("You have rolled: %s." % dice)
+            if len(saved_dice) >= 1:
+                print("Your dice set aside:", saved_dice)
             if roll == 2:
                 roll += 1
                 break
@@ -130,20 +134,70 @@ def main():
             while keep_going.lower() != "stop" and keep_going.lower() != "roll":
                 print("Invalid input.")
                 keep_going = input("Would you like to roll again? Insert 'roll' if yes, 'stop' if you would like to use these dice.\n> ")
-        if keep_going.lower() == "stop":
+            if keep_going == 'roll' and len(dice) >= 1:
+                if len(saved_dice) >= 1:
+                    return_dice_nums = input("Would you like to reroll any saved dice? Insert in format 1, 2, 3, 4 or press enter if none.\n> ")
+                    return_dice_nums = return_dice_nums.split(', ')
+                    valid = False
+                    if return_dice_nums[-1] != '': #check that there are valid values in return_dice
+                        while valid == False: #make sure user input within range for returning dice
+                            wrong = 0
+                            for die_num in return_dice_nums:
+                                if int(die_num) > len(dice):
+                                    return_dice_nums = input("Invalid input. Which of these dice would you like to reroll? Insert in format 1, 2, 3, 4, 5 or press enter if none.\n> ")
+                                    return_dice_nums = return_dice_nums.split(', ')
+                                    wrong += 1
+                            if wrong == 0:
+                                valid = True
+                    subtractor = 0
+                    for die_num in return_dice_nums: #put dice set aside back into rolled dice list
+                        if die_num == '':
+                            continue
+                        indexed = saved_dice.pop(int(die_num) - (1 + subtractor))
+                        dice.append(indexed)
+                        subtractor += 1
+            if keep_going == 'roll' and len(dice) > 1:
+                saved_dice_nums = input("Which of these dice would you like to set aside? Insert in format 1, 2, 3, 4, 5 or press enter if none.\n> ")
+                saved_dice_nums = saved_dice_nums.split(', ')
+                valid = False
+                if saved_dice_nums[-1] != '':
+                    while valid == False:
+                        wrong = 0
+                        for die_num in saved_dice_nums:
+                            if int(die_num) > len(dice):
+                                saved_dice_nums = input("Invalid input. Which of these dice would you like to set aside? Insert in format 1, 2, 3, 4, 5 or press enter if none.\n> ")
+                                saved_dice_nums = saved_dice_nums.split(', ')
+                                wrong += 1
+                        if wrong == 0:
+                            valid = True
+                subtractor = 0
+                for die_num in saved_dice_nums:
+                    if die_num == '':
+                        continue
+                    indexed = dice.pop(int(die_num) - (1 + subtractor))
+                    saved_dice.append(indexed)
+                    subtractor += 1
+        if keep_going.lower() == "stop" or roll == 3:
+            dice.extend(saved_dice)
+            print("Your dice:", dice)
             score_category = input("What category would you like this to be scored under?\n> ")
-        while score_category.lower() not in check_if_category:
-            score_category = input("Invalid input. What category would you like this to be scored under?\n> ")
-        while score_category.lower() in used_score_categories:
-            score_category = input("You have already used this category. Please pick another.\n> ")
-        used_score_categories.append(score_category.lower())
-        scored = score(dice, score_category) 
-        for element in score_board:
-            if element[0] == score_category.lower():  #match player input with category in score board list 
-                category_index = score_board.index(element)
-        score_board[category_index][1] = scored
+            while score_category.lower() not in check_if_category:
+                score_category = input("Invalid input. What category would you like this to be scored under?\n> ")
+            while score_category.lower() in used_score_categories:
+                score_category = input("You have already used this category. Please pick another.\n> ")
+            used_score_categories.append(score_category.lower())
+            scored = score(dice, score_category) 
+            for element in score_board: #Putting scores into board
+                if element[0] == score_category.lower():
+                    category_index = score_board.index(element)
+            score_board[category_index][1] = scored
+            if scored == 0:
+                score_board[category_index][1] = 'X'
         print("Your current score board: %s\n" % score_board)
+        saved_dice = []
     for category in score_board:
+        if category[1] == 'X':
+            continue
         total_score += category[1]
     for category in range(6):
         bonus += score_board[category][1]
@@ -152,5 +206,7 @@ def main():
     print("Upper total: %s" % bonus)
     if bonus >= 63:
         print("You have received a bonus of 35 points! Way to go, you!")
+    else:
+        print("No bonus for you :(")
     print("Your total score is: %s" % total_score)
 main()
